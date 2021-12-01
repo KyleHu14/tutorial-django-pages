9ef43981fef7590e65acded25362a3865cc0a41a
KyleHu14 <kylebusiness1688@gmail.com>
2021-11-30 22:07:16 -0800

    Last Commit

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
